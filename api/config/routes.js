34a54003b37c377cf08a5081ed5d56905ec17768
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-07-26 13:59:56 -0300

    Configure mongoDB and add User model and controller

--- api/config/routes.js
+++ api/config/routes.js
@@ -34,7 +34,8 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'post /User': 'UserController.create'
 
   /***************************************************************************
   *                                                                          *
