b2dad1d46d17518b061846674036f85668c09cee
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-07-26 19:55:01 -0300

    Prepare ChatController to send messages

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -2,23 +2,54 @@ angular.module('starter.controllers', ['btford.socket-io'])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatController', function($scope, $stateParams, $http, socket, $ionicScrollDelegate) {
+  $scope.messages = [];
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+  socket.on('connect',function(){
+    //Add user called nickname
+    socket.emit('add user',$scope.user.name);
+  });
+  socket.on('new message', function (data) {
+    addMessageToList(data.username, true, data.message)
+  });
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+  //function called when user hits the send button
+  $scope.sendMessage = function() {
+    console.log('post message');
+    $http.post('http://localhost:1337/chat', {message: $scope.message, time: new Date()}).
+    success(function(data, status, headers, config) {
+      socket.emit('new message', data);
+    }).
+    error(function(data, status, headers, config) {
+
+    });
+    addMessageToList($stateParams.nickname, true, $scope.message)
+    socket.emit('stop typing');
+  }
+
+
+  function addMessageToList(username, style_type, message){
+    // username = $sanitize(username) //The input is sanitized For more info read this link
+    $scope.messages.push({content: message,style:style_type,username:username})  // Push the messages to the messages list.
+    $ionicScrollDelegate.scrollBottom(); // Scroll to bottom to read the latest
+  }
+
+  // Whenever the server emits 'user joined', log it in the chat body
+  socket.on('user joined', function (data) {
+    addMessageToList("",false,data.username + " joined")
+    addMessageToList("",false,message_string(data.numUsers)) 
+  });
+
+  // Whenever the server emits 'user left', log it in the chat body
+  socket.on('user left', function (data) {
+    addMessageToList("",false,data.username+" left")
+    addMessageToList("",false,message_string(data.numUsers))
+  });
+
+  // Return message string depending on the number of users
+  function message_string(number_of_users) {
+    return number_of_users === 1 ? "there's 1 participant":"there are " + number_of_users + " participants"
+  }
 })
 
 .controller('AccountCtrl', function($scope) {
